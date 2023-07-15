5cc2c419a9668a123446612110d59be3af0ff852
TheAifam5 <theaifam5@gmail.com>
2023-07-16 01:00:41 +0200

    fix(formula/fastboot-edl): change urls of resources

--- Formula/fastboot-edl.rb
+++ Formula/fastboot-edl.rb
@@ -1,10 +1,20 @@
 class FastbootEdl < Formula
-  PLATFORM_SDK_VERSION = "34.0.3".freeze
+  LIBBUILDVERSION_COMMIT_ID = "61d6748027ad43dee7bf96d26b2cd59dec0fed64".freeze
+  LIBBASE_COMMIT_ID = "270063f54b2b1152367a76cd018eb8c155bc6d98".freeze
+  CORE_COMMIT_ID = "a24e8f972f52a903b898fe7a8223f06bd86736eb".freeze
+  LIBZIPARCHIVE_COMMIT_ID = "8fc2060e5cab634ac87e453c24f4d404e8de1c97".freeze
+  LOGGING_COMMIT_ID = "a95edfc827a4f1038776bf9e8951fe0c7f7ded2b".freeze
+  EXTRAS_COMMIT_ID = "f0d2b53d1d887b61e635e81db72a8da23fbcb1fd".freeze
+  MKBOOTIMG_COMMIT_ID = "6aa8ca743fad98954aba9994ac5178e9c63b1c1d".freeze
+  EXTERNAL_FMTLIB_COMMIT_ID = "695001ee76a2d3fb5a8ae71caa8d80bb8b780592".freeze
+  EXTERNAL_AVB_COMMIT_ID = "f45d8524cb404170f6c629beb87dadc5777c46e7".freeze
+  EXTERNAL_GOOGLETEST_COMMIT_ID = "52f1f6e0fcc35f9f3e0a9c8ef1385fdd0dbc2e14".freeze
 
   desc "Fastboot protocol for communicating with bootloaders over USB or ethernet"
   homepage "https://android.googlesource.com/platform/system/core/+/master/fastboot/"
-  url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/fastboot.tar.gz"
-  sha256 "e583c8a1a7bdae22e8f5903a5e7581481857cd32ae5c145f3c8b29425dfe0622"
+  url "https://android.googlesource.com/platform/system/core/+archive/b2631b9ec54fc26a87b1ae8f603f7a899d090616/fastboot.tar.gz"
+  version "34.0.3"
+  sha256 "696b83a184790d810679a604e0b39ffe5b23ff467917a50b70ed85215e0ac3bc"
   license "Apache-2.0"
 
   livecheck do
@@ -14,74 +24,74 @@ class FastbootEdl < Formula
   depends_on "openssl@3"
   depends_on "zlib"
 
+  resource "libbuildversion" do
+    url "https://android.googlesource.com/platform/build/soong/+archive/#{LIBBUILDVERSION_COMMIT_ID}/cc/libbuildversion.tar.gz"
+    sha256 "2c6d9e11aa454a7c6df496041d1bf403058ce7a2991580492fdfcc59b0df4b0f"
+  end
+
   resource "libbase" do
-    url "https://android.googlesource.com/platform/system/libbase/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "7eb477832f3dabbb4937b189ef939567a3d3471664eab72f5ca77515358073f3"
+    url "https://android.googlesource.com/platform/system/libbase/+archive/#{LIBBASE_COMMIT_ID}.tar.gz"
+    sha256 "85ad09d751baf571be4daf7604ae55f17c786cb3637bcf3e18b63432e133f290"
   end
 
   resource "liblp" do
-    url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/fs_mgr/liblp.tar.gz"
-    sha256 "0431b5809d25c5f83c6ef798aa2258b10bed4d4ffc180b7a1ab96b953f555649"
+    url "https://android.googlesource.com/platform/system/core/+archive/#{CORE_COMMIT_ID}/fs_mgr/liblp.tar.gz"
+    sha256 "e353e8312cac628d8c4306519f6d495c20ecb0b24f25f3e538c4f7daec0fa5dd"
   end
 
   resource "libsparse" do
-    url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/libsparse.tar.gz"
-    sha256 "efd5d87638fee898bd90a5db313840cb74c5c37d6cd71d56af72d72d40caf227"
+    url "https://android.googlesource.com/platform/system/core/+archive/#{CORE_COMMIT_ID}/libsparse.tar.gz"
+    sha256 "3153e612bb249c919b79f7cb3e1bda92602d7f13d6e60ceddbf137d5dcf5cacd"
   end
 
   resource "libcutils" do
-    url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/libcutils.tar.gz"
-    sha256 "93dac0f88d75b0cde26932b22a3e908ce368900395fd03ac284f3a7be247b7de"
+    url "https://android.googlesource.com/platform/system/core/+archive/#{CORE_COMMIT_ID}/libcutils.tar.gz"
+    sha256 "864024162ddfc84a6b7606241ac9088bd4d054620da9e67e2e2eafc0e0eda7e9"
   end
 
   resource "libstorage_literals" do
-    url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/fs_mgr/libstorage_literals.tar.gz"
-    sha256 "067e78f8a9dafeaf713d0e350b8ff1eb232437b181921725d73fad95bc5668b8"
-  end
-
-  resource "libfmt" do
-    url "https://android.googlesource.com/platform/external/fmtlib/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "f840bc92b9bebcade68004c8e0256118c0852c837a5df76da64cba832d45b43c"
-  end
-
-  resource "libavb" do
-    url "https://android.googlesource.com/platform/external/avb/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "c9cf12c25c5881679dba0d4d032281ddc36e9c94ff69b678367b204ff360bee9"
-  end
-
-  resource "libbuildversion" do
-    url "https://android.googlesource.com/platform/build/soong/+archive/refs/tags/platform-tools-34.0.3/cc/libbuildversion.tar.gz"
-    sha256 "fef13734d844606a6fe2e7ec54455880f36c3bee7bfdc0f4e44a0758282c871e"
-  end
-
-  resource "libziparchive" do
-    url "https://android.googlesource.com/platform/system/libziparchive/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "4228431f7df9d89f750cafe1142caebc3bd906dca889b53dcf14c6731308fa19"
+    url "https://android.googlesource.com/platform/system/core/+archive/#{CORE_COMMIT_ID}/fs_mgr/libstorage_literals.tar.gz"
+    sha256 "37abdb89faae2e74ac3cf69dc226a108e92f35ed2a7a050685786599eeada6ae"
   end
 
   resource "diagnose_usb" do
-    url "https://android.googlesource.com/platform/system/core/+archive/refs/tags/platform-tools-34.0.3/diagnose_usb.tar.gz"
-    sha256 "368e7df20077c18cbf19341706b7865b3f7f0fbfdb64ce3bc4b3fce027f86d72"
+    url "https://android.googlesource.com/platform/system/core/+archive/#{CORE_COMMIT_ID}/diagnose_usb.tar.gz"
+    sha256 "7ce5dc5b0a8dd486c817302f7941668d52a6a59bf18ecd49c74faa951022030d"
+  end
+
+  resource "libziparchive" do
+    url "https://android.googlesource.com/platform/system/libziparchive/+archive/#{LIBZIPARCHIVE_COMMIT_ID}.tar.gz"
+    sha256 "ca992e60dc7f22baf33a2c8cf941903f83952488004b13c35f5c9a80bc990a8d"
   end
 
   resource "liblog" do
-    url "https://android.googlesource.com/platform/system/logging/+archive/refs/tags/platform-tools-34.0.3/liblog.tar.gz"
-    sha256 "c88bd6cd1e20bb95dfc68200b4619ef621e52fc3e553d6727f31e4244f74a1e3"
+    url "https://android.googlesource.com/platform/system/logging/+archive/#{LOGGING_COMMIT_ID}/liblog.tar.gz"
+    sha256 "d9f4289858b05d491bcf7808451fa30e9a6f9f14e8753fd3814ad29ac938894f"
   end
 
   resource "ext4_utils" do
-    url "https://android.googlesource.com/platform/system/extras/+archive/refs/tags/platform-tools-34.0.3/ext4_utils.tar.gz"
-    sha256 "bc71f0bffd92139db53171e0dd321081a141f492c16c5248cc4e3e7d12780341"
-  end
-
-  resource "googletest" do
-    url "https://android.googlesource.com/platform/external/googletest/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "3463c53c8fdad927888f2ab839c642f7f44aefc6f02764c352f79248fbf756cc"
+    url "https://android.googlesource.com/platform/system/extras/+archive/#{EXTRAS_COMMIT_ID}/ext4_utils.tar.gz"
+    sha256 "db0329e9ff8224e877cc7b227fb4cd0a9c721f35a01ff882d98bdf4922c11c18"
   end
 
   resource "mkbootimg" do
-    url "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/tags/platform-tools-34.0.3.tar.gz"
-    sha256 "18116b96e05ad6936566d2030eb8c7e1a49350c45ef9ac13d9270fb68c778109"
+    url "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/#{MKBOOTIMG_COMMIT_ID}.tar.gz"
+    sha256 "8f5847510fddc88f3ec7538ac58122968183d29f96430822eedafb64691381af"
+  end
+
+  resource "libfmt" do
+    url "https://android.googlesource.com/platform/external/fmtlib/+archive/#{EXTERNAL_FMTLIB_COMMIT_ID}.tar.gz"
+    sha256 "7db1b58543d4983b459c4659966ff5cb60efc7f858f7825e7ca703704c1c7902"
+  end
+
+  resource "libavb" do
+    url "https://android.googlesource.com/platform/external/avb/+archive/#{EXTERNAL_AVB_COMMIT_ID}.tar.gz"
+    sha256 "c4d1e1bf086eccfc3cb029fd8a0d73064e119adbe112eb5616ed7d05ea447333"
+  end
+
+  resource "googletest" do
+    url "https://android.googlesource.com/platform/external/googletest/+archive/#{EXTERNAL_GOOGLETEST_COMMIT_ID}.tar.gz"
+    sha256 "696b83a184790d810679a604e0b39ffe5b23ff467917a50b70ed85215e0ac3bc"
   end
 
   def install
@@ -102,7 +112,7 @@ class FastbootEdl < Formula
 
     (buildpath/"mkbootimg").install resource("mkbootimg")
 
-    (buildpath/"platform_tools_version.h").write "#define PLATFORM_TOOLS_VERSION \"#{PLATFORM_SDK_VERSION}\""
+    (buildpath/"platform_tools_version.h").write "#define PLATFORM_TOOLS_VERSION \"#{version}\""
 
     cd "libziparchive" do
       inreplace "zip_archive.cc", "zstream.next_in = buf;", "zstream.next_in = (Bytef *)buf;"
